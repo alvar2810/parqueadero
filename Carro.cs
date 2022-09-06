6ef3b11191317ba28282db2a9c352a3314e359e0
pinnul <102242533+pinnul@users.noreply.github.com>
2022-09-05 19:21:55 -0500

    Adiccionado comentarios

--- Carro.cs
+++ Carro.cs
@@ -1,3 +1,4 @@
+/// <summary> vehiculo que se parquea </summary>
 public class carro {
     public string placa;
     public string marca;
---
+++ CarroElectrico.cs
@@ -0,0 +1,4 @@
+/// <summary> Carro priorizado por ser electrico </summary>
+public class CarroElectrico:Carro {
+    public float DuraccionBateria {get; set;}
+}
